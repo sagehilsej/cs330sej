143d781fbd0330a15b9d1fc8f5490bde6375ac11
sagehilsej <98236754+sagehilsej@users.noreply.github.com>
2022-10-19 03:10:17 -0400

    Create date.js

---
+++ Lab02/date.js
@@ -0,0 +1,56 @@
+const popup = document.querySelector("#popup");
+const onpage = document.querySelector("#onpage");
+const output = document.querySelector("#output");
+const format = document.querySelector("#format");
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const d = new Date();
+function getMydate() {
+  let date =
+    "The date today is " +
+    months[d.getMonth()] +
+    " " +
+    d.getDate() +
+    ", " +
+    d.getFullYear() +
+    "\n";
+
+  return date;
+}
+function get12Hour() {
+  let hour = d.getHours() % 12 || 12;
+  let min = d.getMinutes() > 10 ? d.getMinutes() : "0" + d.getMinutes();
+  let time = "The time is " + hour + ":" + min;
+  return time;
+}
+function get24Hour() {
+  let min = d.getMinutes() > 10 ? d.getMinutes() : "0" + d.getMinutes();
+  let time = "The time is " + d.getHours() + ":" + min;
+  return time;
+}
+popup.onclick = function () {
+  if (format.value == "12") {
+    alert(getMydate() + get12Hour());
+  } else {
+    alert(getMydate() + get24Hour());
+  }
+};
+onpage.onclick = function () {
+  if (format.value == "12") {
+    output.innerHTML = getMydate() + "<br>" + get12Hour();
+  } else {
+    output.innerHTML = getMydate() + "<br>" + get24Hour();
+  }
+};
